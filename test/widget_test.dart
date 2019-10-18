77f9955cff356ba3d85d65ed30d14983d399be1a
Javi Carrion <suavejavi@google.com>
2019-10-17 23:21:27 -0400

    feat: add iOS icons for future use

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,23 +5,23 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:devfestcenfl/home/index.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Schedule is present and trigger navigation after tap - smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     //await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+   await tester.pumpWidget(MaterialApp(home: HomePage()));
+
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+   
 
+    expect(find.byType(RaisedButton), findsOneWidget);
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
